3241d93cde79847dab400505e6e8c92d7d4bd6fd
Tom Wayson <tom@tomwayson.com>
2018-03-06 12:28:31 -0800

    replace mock feature query w/ queryFeatures()

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,6 +5,7 @@ import 'preact-material-components/List/style.css';
 import 'preact-material-components/TextField/style.css';
 import { bind, debounce } from 'decko';
 import { route } from 'preact-router';
+import { queryFeatures } from '@esri/arcgis-rest-feature-service';
 
 import style from './style';
 
@@ -18,69 +19,25 @@ export default class Home extends Component {
 	@debounce(500)
 	searchEvents(e) {
 		const newSearch = e.target.value;
-		const results = {
-			features: [{
-	attributes: {
-	OBJECTID: 164,
-	title: "testing",
-	location: "Courthouse, Virginia",
-	description: "testing",
-	startDate: 1527360300000,
-	endDate: 1527361200000,
-	organizerId: null,
-	organizerName: null,
-	organizerEmail: null,
-	url: "testing",
-	pageId: "2794ec2e618246f19b48f0bffd2575ac",
-	capacity: null,
-	attendance: null,
-	status: "public",
-	isCancelled: null,
-	groupId: "516be607ee6042a89440f401ab6bca89",
-	siteId: "b89964dcb21a4e7eb9d97b47556ff572",
-	initiativeId: "9cb9eee39b1947e0a0d18249df942995",
-	surveyId: null,
-	CreationDate: 1517510810767,
-	Creator: "dcadminqa",
-	EditDate: 1518188455371,
-	Editor: "dcadminqa"
-	}
-	},
-	{
-	attributes: {
-	OBJECTID: 168,
-	title: "davidson event",
-	location: "ESRI",
-	description: "Doing the thing",
-	startDate: 1527354000000,
-	endDate: 1527357600000,
-	organizerId: null,
-	organizerName: null,
-	organizerEmail: null,
-	url: "davidson-event",
-	pageId: "7aacf725e0d34d34bdf3c0b6c0dcfd90",
-	capacity: null,
-	attendance: null,
-	status: "public",
-	isCancelled: null,
-	groupId: "02e7c55e724540bf87ce9c8b60cb0519",
-	siteId: "b89964dcb21a4e7eb9d97b47556ff572",
-	initiativeId: "9cb9eee39b1947e0a0d18249df942995",
-	surveyId: null,
-	CreationDate: 1517937966046,
-	Creator: "dcadminqa",
-	EditDate: 1518188451941,
-	Editor: "dcadminqa"
-	}
-	}]
-		};
-		setTimeout(() => {
-			this.setState({
-				search: newSearch,
-				eventFeatures: results.features
+		this.queryEvents(newSearch)
+			.then(response => {
+				this.setState({
+					search: newSearch,
+					eventFeatures: response.features
+				});
 			});
-		}, 300);
-	}
+	};
+
+	queryEvents(search) {
+		const featureLayerUrl = 'https://servicesqa.arcgis.com/97KLIFOSt5CxbiRI/ArcGIS/rest/services/Hub%20Events%20(public)/FeatureServer/0';
+		const queryParams = {
+			outFields: ['OBJECTID', 'title', 'startDate']
+		};
+		if (search) {
+			queryParams.where = `title LIKE '%${search}%'`;
+		}
+		return queryFeatures({ url: featureLayerUrl, params: queryParams });
+	};
 
 	goToEvent(eventId) {
 		route(`/event/${eventId}`, true);
